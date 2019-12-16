0874be73d9f964729a53d98ea509372ea8b1a96b
Huxifan <xifan_1995@163.com>
2019-12-16 10:56:09 +0800

    hello flask

--- hello.py
+++ hello.py
@@ -3,10 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 
+# 创建flask的应用对象
+# __name__表示当前的模块名字
+# 模块名，flask以这个模块所在的目录为总目录，默认这个目录中的static为静态目录，templates为模板目录
 @app.route('/')
 def index():
     return 'hello flask'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run()  # 启动flask四程序
